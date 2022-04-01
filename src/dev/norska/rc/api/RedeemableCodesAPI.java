655d361e1294d804695f9854eedbe7afa980ee88
Norska <norska99@gmail.com>
2022-04-01 11:28:47 +0300

    Build 36a Release

--- src/dev/norska/rc/api/RedeemableCodesAPI.java
+++ src/dev/norska/rc/api/RedeemableCodesAPI.java
@@ -1,41 +1,38 @@
 package dev.norska.rc.api;
 
-import dev.norska.rc.RedeemableCodes;
 import dev.norska.rc.data.Code;
 
 public class RedeemableCodesAPI {
 	
 	/** 
 	* Get a Code and access it's settings.
-	* You can get the instance of RedeemableCodes with RedeemableCodes.getInstance();.
-	* @param RedeemableCodes Instance, String
+	* @param String Identifier
 	* @return Code
 	*/
-	public static Code getCode(RedeemableCodes instance, String identifier) {
+	public static Code getCode(String identifier) {
 		return null;
 	}
 	
 	/** 
 	* Create a Code.
-	* @param RedeemableCodes Instance, String
+	* @param String Identifier
 	*/
-	public static void createCode(RedeemableCodes instance, String identifier) {
+	public static void createCode(String identifier) {
 		
 	}
 	
 	/** 
 	* Delete a Code.
-	* @param RedeemableCodes Instance, String
+	* @param String Identifier
 	*/
-	public static void deleteCode(RedeemableCodes instance, String identifier) {
+	public static void deleteCode(String identifier) {
 	
 	}
 	
 	/** 
 	* Delete all Codes.
-	* @param RedeemableCodes Instance
 	*/
-	public static void deleteAllCodes(RedeemableCodes instance) {
+	public static void deleteAllCodes() {
 	
 	}
 
--- src/dev/norska/rc/data/Code.java
+++ src/dev/norska/rc/data/Code.java
@@ -7,9 +7,10 @@ import lombok.Setter;
 
 public class Code {
 
-	@Getter @Setter private List<String> commands;
-	@Getter @Setter private List<String> securedToUUIDs, usedByUUIDs;
+	@Getter @Setter private List<String> commands, commandIDTabComplete;
+	@Getter @Setter private List<String> securedToUUIDs, usedByUUIDs, securedUUIDTabComplete;
 	@Getter @Setter private int uses;
-	@Getter @Setter private Boolean permBased;
+	@Getter @Setter private Boolean permBased, enabled;
+	@Getter @Setter private String expiry;
 	
 }
